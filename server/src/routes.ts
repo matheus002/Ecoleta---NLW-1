414f47b284e5ba3500167d8a563295fecb914795
Math002 <matheus002_@hotmail.com>
2020-07-08 14:44:52 -0300

    add files

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,46 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+routes.get('/items', async (req, res) => {
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map((item) => {
+    return {
+      id: item.id,
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
+  });
+
+  return res.json(serializedItems);
+});
+
+routes.post('/points', async (req, res) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items,
+  } = req.body;
+
+  knex('points').insert({
+    image: 'image_fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  });
+
+  return res.json({ success: true });
 });
 
 export default routes;
